53298e2686922ac7a0d28abd215cab2602918c12
Kevin Hu <hukevin69@gmail.com>
2024-07-17 14:23:02 -0700

    option to change memory allocation to the container

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -8,6 +8,7 @@ exec /usr/bin/java \
   -Dawt.useSystemAAFontSettings=gasp \
   -Djava.awt.headless=false \
   -XX:-UsePerfData \
+  -Xmx${MEMORY:-"4G"} \
   -jar /config/mcaselector.jar
 
 # vim:ft=sh:ts=4:sw=4:et:sts=4
